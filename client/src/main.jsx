2d03a40dee64f411a32b9bc609c8c0df15db3b02
SophoGurabanidze <sogurabanidze@gmail.com>
2025-06-24 10:41:33 +0400

    fix: remove react-helmet-async and clean up main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,6 @@ import "leaflet/dist/leaflet.css";
 import App from "./App.jsx";
 import { LoadScript } from "@react-google-maps/api";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { HelmetProvider } from "react-helmet-async";
 import "./i18n";
 
 const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
@@ -15,9 +14,7 @@ createRoot(document.getElementById("root")).render(
   <LoadScript googleMapsApiKey={apiKey} language="ka" region="GE">
     <StrictMode>
       <QueryClientProvider client={queryClient}>
-        <HelmetProvider>
-          <App />
-        </HelmetProvider>
+        <App />
       </QueryClientProvider>
     </StrictMode>
   </LoadScript>
